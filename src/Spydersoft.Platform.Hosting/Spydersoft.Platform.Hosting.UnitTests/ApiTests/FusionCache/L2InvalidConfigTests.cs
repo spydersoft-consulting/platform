197125b66a834b04edad621015dc3f02f63a7d3e
Matt Gerega <geregam@gmail.com>
2025-07-29 21:05:28 -0400

    Fix unnecessary async

--- src/Spydersoft.Platform.Hosting/Spydersoft.Platform.Hosting.UnitTests/ApiTests/FusionCache/L2InvalidConfigTests.cs
+++ src/Spydersoft.Platform.Hosting/Spydersoft.Platform.Hosting.UnitTests/ApiTests/FusionCache/L2InvalidConfigTests.cs
@@ -5,7 +5,7 @@ namespace Spydersoft.Platform.Hosting.UnitTests.ApiTests.FusionCache;
 public class L2InvalidConfigTests
 {
 	[Test]
-	public async Task StartupFails_NoRedisConnectionString()
+	public void StartupFails_NoRedisConnectionString()
 	{
         var factory = new UnitTestWebApplicationFactory("FusionCacheL2NoRedis");
         Assert.Throws<ArgumentException>(() => 

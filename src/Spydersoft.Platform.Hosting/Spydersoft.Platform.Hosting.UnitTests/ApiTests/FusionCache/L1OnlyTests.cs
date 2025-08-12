2b2e471489baa71bcf2d41edf7463dcf7c16603f
Matt Gerega <geregam@gmail.com>
2025-08-11 22:26:42 -0400

    Add additional cache tests

--- src/Spydersoft.Platform.Hosting/Spydersoft.Platform.Hosting.UnitTests/ApiTests/FusionCache/L1OnlyTests.cs
+++ src/Spydersoft.Platform.Hosting/Spydersoft.Platform.Hosting.UnitTests/ApiTests/FusionCache/L1OnlyTests.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics;
 using System.Net;
 using System.Text.Json;
 
 namespace Spydersoft.Platform.Hosting.UnitTests.ApiTests.FusionCache;
+
 public class L1OnlyCacheTests : ApiTestBase
 {
 	public override string Environment => "FusionNoCache";
@@ -24,11 +26,50 @@ public class L1OnlyCacheTests : ApiTestBase
 	{
 		var result = await Client.GetAsync($"/cachetest/HasDistributedCache");
 
-        Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-        var content = await result.Content.ReadAsStringAsync();
+		Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+		var content = await result.Content.ReadAsStringAsync();
 
 		var firstResult = JsonSerializer.Deserialize<dynamic>(content, JsonOptions);
-        Assert.That(firstResult, Is.Not.Null);
-        Assert.That(firstResult.GetProperty("hasL2Cache").GetBoolean(), Is.False);
+		Assert.That(firstResult, Is.Not.Null);
+		Assert.That(firstResult.GetProperty("hasL2Cache").GetBoolean(), Is.False);
+	}
+	
+		[Test]
+	public async Task InitialLoad_Occurs()
+	{
+		var stopwatch = Stopwatch.StartNew();
+		var result = await Client.GetAsync($"/cachetest/97");
+		stopwatch.Stop();
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(stopwatch.ElapsedMilliseconds, Is.InRange(2000, 5000), "Initial load should take longer than 2 seconds due to cache population delay, but less than 5 seconds, which is the load without cache.");
+			Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			var content = await result.Content.ReadAsStringAsync();
+
+			var firstResult = JsonSerializer.Deserialize<dynamic>(content, JsonOptions);
+			Assert.That(firstResult, Is.Not.Null);
+		}
+	}
+	
+	[Test]
+	public async Task CacheLoad_Occurs()
+	{
+		// First call populates the cache.
+		_ = await Client.GetAsync($"/cachetest/98");
+
+		var stopwatch = Stopwatch.StartNew();
+		var result = await Client.GetAsync($"/cachetest/98");
+		stopwatch.Stop();
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(stopwatch.ElapsedMilliseconds, Is.LessThan(2000), "Second Load should take less than 2 seconds.");
+			Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			var content = await result.Content.ReadAsStringAsync();
+
+			var firstResult = JsonSerializer.Deserialize<dynamic>(content, JsonOptions);
+			Assert.That(firstResult, Is.Not.Null);
+		}
 	}
 }
--- src/Spydersoft.Platform.Hosting/Spydersoft.Platform.Hosting.UnitTests/ApiTests/FusionCache/L2Tests.cs
+++ src/Spydersoft.Platform.Hosting/Spydersoft.Platform.Hosting.UnitTests/ApiTests/FusionCache/L2Tests.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics;
 using System.Net;
 using System.Text.Json;
 
 namespace Spydersoft.Platform.Hosting.UnitTests.ApiTests.FusionCache;
+
 public class L2Tests : ApiTestBase
 {
 	public override string Environment => "FusionCacheL2";
@@ -24,11 +26,51 @@ public class L2Tests : ApiTestBase
 	{
 		var result = await Client.GetAsync($"/cachetest/HasDistributedCache");
 
-        Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-        var content = await result.Content.ReadAsStringAsync();
+		Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+		var content = await result.Content.ReadAsStringAsync();
 
 		var firstResult = JsonSerializer.Deserialize<dynamic>(content, JsonOptions);
-        Assert.That(firstResult, Is.Not.Null);
-        Assert.That(firstResult.GetProperty("hasL2Cache").GetBoolean(), Is.True);
+		Assert.That(firstResult, Is.Not.Null);
+		Assert.That(firstResult.GetProperty("hasL2Cache").GetBoolean(), Is.True);
 	}
+
+	[Test]
+	public async Task InitialLoad_Occurs()
+	{
+		var stopwatch = Stopwatch.StartNew();
+		var result = await Client.GetAsync($"/cachetest/99");
+		stopwatch.Stop();
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(stopwatch.ElapsedMilliseconds, Is.InRange(2000, 5000), "Initial load should take longer than 2 seconds due to cache population delay, but less than 5 seconds, which is the load without cache.");
+			Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			var content = await result.Content.ReadAsStringAsync();
+
+			var firstResult = JsonSerializer.Deserialize<dynamic>(content, JsonOptions);
+			Assert.That(firstResult, Is.Not.Null);
+		}
+	}
+	
+	[Test]
+	public async Task CacheLoad_Occurs()
+	{
+		// First call populates the cache.
+		_ = await Client.GetAsync($"/cachetest/100");
+
+		var stopwatch = Stopwatch.StartNew();
+		var result = await Client.GetAsync($"/cachetest/100");
+		stopwatch.Stop();
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(stopwatch.ElapsedMilliseconds, Is.LessThan(2000), "Second Load should take less than 2 seconds.");
+			Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			var content = await result.Content.ReadAsStringAsync();
+
+			var firstResult = JsonSerializer.Deserialize<dynamic>(content, JsonOptions);
+			Assert.That(firstResult, Is.Not.Null);
+		}
+	}
+
 }
